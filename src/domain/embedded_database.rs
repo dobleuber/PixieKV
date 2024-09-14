fb0bbcb9247ada358f70739c7dee082e1ad535c8
dobleuber <wbertc@gmail.com>
2024-09-13 21:42:32 -0500

    remove unnecessary key value struct

--- src/domain/embedded_database.rs
+++ src/domain/embedded_database.rs
@@ -3,16 +3,10 @@ use alloc::string::String;
 
 use core::result::Result;
 
-
-pub struct KeyValuePair<T: Sized> {
-    key: String,
-    value: T,
-}
-
 const MAX_SIZE: usize = 1024;
 
 pub struct EmbeddedDatabase<T: Sized> {
-    data: IndexMap<String, KeyValuePair<T>, MAX_SIZE>,
+    data: IndexMap<String, T, MAX_SIZE>,
 }
 
 impl<T: Sized> Default for EmbeddedDatabase<T> {
@@ -31,17 +25,17 @@ pub trait Database<T: Sized> {
 
 impl<T: Sized> Database<T> for EmbeddedDatabase<T> {
     fn insert(&mut self, key: String, value: T) -> Result<(), String> {
-        self.data.insert(key.clone(), KeyValuePair { key, value })
+        self.data.insert(key.clone(), value )
             .map(|_| ())
             .map_err(|_| String::from("Database is full"))
     }
 
     fn get(&self, key: &str) -> Option<&T> {
-        self.data.get(key).map(|kv| &kv.value)
+        self.data.get(key)
     }
 
     fn remove(&mut self, key: &str) -> Option<T> {
-        self.data.remove(key).map(|kv| kv.value)
+        self.data.remove(key)
     }
 }
 #[cfg(test)]
